4ed120ea71af16a195838527b9ea5d7cf073cfe8
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-01 00:14:25 -0300

    Add AddTransactionActivity without implementation

---
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionActivity.kt
@@ -0,0 +1,12 @@
+package br.com.walter.walter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addtransaction_activity)
+    }
+}
