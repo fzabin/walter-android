214de5e3f74d528bd93109a6914efbae6770c418
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-01 00:33:10 -0300

    Add fab button to navigate to add production screen

--- app/src/main/kotlin/br/com/walter/walter/features/home/presentation/HomeActivity.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/home/presentation/HomeActivity.kt
@@ -1,13 +1,24 @@
 package br.com.walter.walter.features.home.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.walter.walter.AddTransactionActivity
 import br.com.walter.walter.R
+import kotlinx.android.synthetic.main.home_activity.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+
+        home_add_fab.setOnClickListener { navigateToAddTransaction() }
     }
+
+    private fun navigateToAddTransaction() {
+        val intent = Intent(this, AddTransactionActivity::class.java)
+        startActivity(intent)
+    }
+
 }
