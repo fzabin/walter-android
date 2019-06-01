ef2eb793e3ce4a5af7611d6f484bb7d15e4a8760
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-01 08:57:29 -0300

    Add transaction type dto

---
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/data/TransactionTypeDto.kt
@@ -0,0 +1,36 @@
+package br.com.walter.walter.features.addtransaction.data
+
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import br.com.walter.walter.core.functional.TwoWayMapper
+import br.com.walter.walter.features.addtransaction.TransactionType
+
+const val TRANSACTION_TYPE_TABLE_NAME = "transaction_type"
+
+@Entity(tableName = TRANSACTION_TYPE_TABLE_NAME )
+data class TransactionTypeDto(
+    @PrimaryKey
+    val id: Long,
+    val description: String
+) {
+    @Ignore
+    constructor() : this(0, "")
+}
+
+class TransactionTypeDtoMapper : TwoWayMapper<TransactionTypeDto, TransactionType> {
+
+    override fun map(param: TransactionTypeDto): TransactionType = with(param) {
+        TransactionType(
+            id = id,
+            description = description
+        )
+    }
+
+    override fun mapReverse(param: TransactionType): TransactionTypeDto = with(param) {
+        TransactionTypeDto(
+            id = id,
+            description = description
+        )
+    }
+}
