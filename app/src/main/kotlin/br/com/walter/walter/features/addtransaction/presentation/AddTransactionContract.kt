b94547aed58a3b8639bf13d65860341136e0b12f
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-02 12:23:56 -0300

    Add add transaction contract

---
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionContract.kt
@@ -0,0 +1,20 @@
+package br.com.walter.walter.features.addtransaction.presentation
+
+import br.com.walter.walter.core.platform.base.BasePresenter
+import br.com.walter.walter.core.platform.base.BaseView
+
+interface AddTransactionContract {
+
+    interface View: BaseView<Presenter> {
+
+        fun showMessage(message: String)
+
+    }
+
+    interface Presenter: BasePresenter {
+
+        fun getAllTransactionTypes()
+
+    }
+
+}
