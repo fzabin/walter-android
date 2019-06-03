33f978c6769b9d4be38a3a2689061d8c60c85bc6
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-03 16:39:45 -0300

    Add database migration from 1 to 2
    Add category table and category table initial data.

--- app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabaseMigration.kt
+++ app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabaseMigration.kt
@@ -2,9 +2,29 @@ package br.com.walter.walter.core.persistence
 
 import android.util.Log
 import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import br.com.walter.walter.features.addtransaction.data.CATEGORY_INITIAL_SETUP
 
 fun getDatabaseMigrations(): Array<Migration> =
-    arrayOf()
+    arrayOf(
+        MigrationFrom1To2
+    )
+
+object MigrationFrom1To2 : Migration(DATABASE_VERSION_1, DATABASE_VERSION_2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        logMigration()
+
+        database.execSQL(
+            "CREATE TABLE IF NOT EXISTS `category` " +
+                    "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`description` TEXT NOT NULL, " +
+                    "`transaction_type_id` INTEGER NOT NULL, " +
+                    "FOREIGN KEY(`transaction_type_id`) REFERENCES `transaction_type`(`id`) ON DELETE CASCADE);"
+        )
+
+        database.execSQL(CATEGORY_INITIAL_SETUP)
+    }
+}
 
 // region Logcat
 private fun Migration.tag(): String = javaClass.simpleName
