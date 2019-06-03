dae7402ffafd6ff1f26d763dbf924c2d3c4e735e
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-03 16:46:18 -0300

    Add method to get all categories to the add transaction presenter

--- app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionPresenter.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionPresenter.kt
@@ -3,23 +3,36 @@ package br.com.walter.walter.features.addtransaction.presentation
 import br.com.walter.walter.core.platform.CoroutinePresenter
 import br.com.walter.walter.core.functional.onFailure
 import br.com.walter.walter.core.functional.onSuccess
+import br.com.walter.walter.features.addtransaction.CategoryRepository
 import br.com.walter.walter.features.addtransaction.TransactionTypeRepository
 import kotlinx.coroutines.launch
 
 class AddTransactionPresenter(
     private val view: AddTransactionContract.View,
-    private val repository: TransactionTypeRepository
-): AddTransactionContract.Presenter, CoroutinePresenter() {
+    private val transactionTypeRepository: TransactionTypeRepository,
+    private val categoryRepository: CategoryRepository
+) : AddTransactionContract.Presenter, CoroutinePresenter() {
 
     override fun start() {
         getAllTransactionTypes()
+        getAllCategories()
     }
 
     override fun getAllTransactionTypes() {
         launch {
-            repository.getAll()
+            transactionTypeRepository.getAll()
                 .onSuccess { transactionTypes ->
                     view.showMessage(transactionTypes.toString())
+                }
+                .onFailure { view.showMessage("Failure!") }
+        }
+    }
+
+    override fun getAllCategories() {
+        launch {
+            categoryRepository.getAll()
+                .onSuccess { categories ->
+                    view.showMessage(categories.toString())
                 }
                 .onFailure { view.showMessage("Failure!") }
         }
