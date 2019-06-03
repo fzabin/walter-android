0c314ec3930a53857f61a3f4cbdeacb5a29ce067
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-03 16:48:20 -0300

    Add migrations to database instance on add transaction activity and fix presenter instantiation

--- app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionActivity.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionActivity.kt
@@ -8,6 +8,9 @@ import br.com.walter.walter.R
 import br.com.walter.walter.core.persistence.AppDatabase
 import br.com.walter.walter.core.persistence.AppDatabaseCallback
 import br.com.walter.walter.core.persistence.DATABASE_NAME
+import br.com.walter.walter.core.persistence.getDatabaseMigrations
+import br.com.walter.walter.features.addtransaction.data.CategoryDataSource
+import br.com.walter.walter.features.addtransaction.data.CategoryDtoMapper
 import br.com.walter.walter.features.addtransaction.data.TransactionTypeDataSource
 import br.com.walter.walter.features.addtransaction.data.TransactionTypeDtoMapper
 
@@ -20,15 +23,23 @@ class AddTransactionActivity : AppCompatActivity(), AddTransactionContract.View 
         setContentView(R.layout.addtransaction_activity)
 
         val database = Room.databaseBuilder(this, AppDatabase::class.java, DATABASE_NAME)
+            .addMigrations(*getDatabaseMigrations())
             .addCallback(AppDatabaseCallback)
             .build()
 
         val transactionTypeDao = database.transactionTypeDao()
         val transactionTypeDtoMapper = TransactionTypeDtoMapper()
 
-        val repository = TransactionTypeDataSource(transactionTypeDao, transactionTypeDtoMapper)
+        val categoryDao = database.categoryDao()
+        val categoryDtoMapper = CategoryDtoMapper()
 
-        presenter = AddTransactionPresenter(view = this, repository = repository)
+        val transactionTypeRepository = TransactionTypeDataSource(transactionTypeDao, transactionTypeDtoMapper)
+        val categoryRepository = CategoryDataSource(categoryDao, categoryDtoMapper)
+
+        presenter = AddTransactionPresenter(
+            view = this,
+            transactionTypeRepository = transactionTypeRepository,
+            categoryRepository = categoryRepository)
         presenter.start()
 
     }
