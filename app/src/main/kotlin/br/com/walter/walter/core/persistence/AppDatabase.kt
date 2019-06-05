1ae6d87c8e8d407997a9cd8391ed566c2c80aab9
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-04 22:26:12 -0300

    Update app database version and add transaction dto and dao

--- app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabase.kt
+++ app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabase.kt
@@ -2,22 +2,21 @@ package br.com.walter.walter.core.persistence
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import br.com.walter.walter.features.addtransaction.data.CategoryDao
-import br.com.walter.walter.features.addtransaction.data.CategoryDto
-import br.com.walter.walter.features.addtransaction.data.TransactionTypeDao
-import br.com.walter.walter.features.addtransaction.data.TransactionTypeDto
+import br.com.walter.walter.features.addtransaction.data.*
 
 const val DATABASE_NAME = "br.com.walter.walter.db"
 
 const val DATABASE_VERSION_1 = 1
 const val DATABASE_VERSION_2 = 2
+const val DATABASE_VERSION_3 = 3
 
 @Database(
     entities = [
         TransactionTypeDto::class,
-        CategoryDto::class
+        CategoryDto::class,
+        TransactionDto::class
     ],
-    version = DATABASE_VERSION_2,
+    version = DATABASE_VERSION_3,
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -25,5 +24,7 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionTypeDao(): TransactionTypeDao
 
     abstract fun categoryDao(): CategoryDao
+
+    abstract fun transactionDao(): TransactionDao
 
 }
