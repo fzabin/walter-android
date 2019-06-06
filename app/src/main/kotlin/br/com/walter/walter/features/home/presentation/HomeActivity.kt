51de0cdec8752a4d294d0c47869c329aa3868d3c
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-05 22:36:28 -0300

    Add methods to setup action bar and status bar on the home activity

--- app/src/main/kotlin/br/com/walter/walter/features/home/presentation/HomeActivity.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/home/presentation/HomeActivity.kt
@@ -3,6 +3,8 @@ package br.com.walter.walter.features.home.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import br.com.walter.walter.features.addtransaction.presentation.AddTransactionActivity
 import br.com.walter.walter.R
 import kotlinx.android.synthetic.main.home_activity.*
@@ -13,7 +15,22 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
+        setupActionBar()
+        setupStatusBarColor()
+
         home_add_fab.setOnClickListener { navigateToAddTransaction() }
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.hide()
+    }
+
+    private fun setupStatusBarColor() {
+        val color = ContextCompat.getColor(this, R.color.colorPrimary)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = color
     }
 
     private fun navigateToAddTransaction() {
