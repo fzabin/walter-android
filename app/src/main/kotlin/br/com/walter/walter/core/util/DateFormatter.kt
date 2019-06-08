2d0905463b2cde60fe03de2765fab02949028a54
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-08 11:20:53 -0300

    Add method to get current date formatted in Brazilian format

--- app/src/main/kotlin/br/com/walter/walter/core/util/DateFormatter.kt
+++ app/src/main/kotlin/br/com/walter/walter/core/util/DateFormatter.kt
@@ -5,14 +5,18 @@ import org.joda.time.format.DateTimeFormat
 
 private const val FORMAT_TZ = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 private const val FORMAT_DISPLAY = "dd/MM/yy, HH:mm"
+private const val FORMAT_BR = "dd/MM/yyyy"
 
 class DateFormatter {
 
     private val dtfTZ by lazy { DateTimeFormat.forPattern(FORMAT_TZ) }
     private val dtfDisplay by lazy { DateTimeFormat.forPattern(FORMAT_DISPLAY) }
+    private val dtfBR by lazy { DateTimeFormat.forPattern(FORMAT_BR) }
 
     fun tzFormatToDisplayFormat(input: String): String =
         dtfDisplay.print(dtfTZ.parseDateTime(input).toDateTime())
 
     fun nowAsTzFormat(): String = dtfTZ.print(DateTime())
+
+    fun nowAsBrFormat(): String = dtfBR.print(DateTime())
 }
