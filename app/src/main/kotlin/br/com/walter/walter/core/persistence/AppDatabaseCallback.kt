8f0254d5c0fb9e8b3a3ffb4c326e8a324f88323f
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-14 23:45:11 -0300

    Add category initial data to app database callback for new installations

--- app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabaseCallback.kt
+++ app/src/main/kotlin/br/com/walter/walter/core/persistence/AppDatabaseCallback.kt
@@ -3,6 +3,7 @@ package br.com.walter.walter.core.persistence
 import android.util.Log
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import br.com.walter.walter.features.addtransaction.data.CATEGORY_INITIAL_SETUP
 import br.com.walter.walter.features.addtransaction.data.TRANSACTION_TYPE_INITIAL_SETUP
 import java.util.concurrent.Executors
 
@@ -12,6 +13,7 @@ object AppDatabaseCallback : RoomDatabase.Callback() {
         try {
             Executors.newSingleThreadExecutor().execute {
                 db.execSQL(TRANSACTION_TYPE_INITIAL_SETUP)
+                db.execSQL(CATEGORY_INITIAL_SETUP)
             }
         } catch (e: Exception) {
             Log.d(AppDatabaseCallback::class.java.simpleName, e.message)
