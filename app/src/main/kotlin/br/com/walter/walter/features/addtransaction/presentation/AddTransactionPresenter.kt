cc767a23f9ff19e237b34ebf96d20a888645567c
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-15 00:22:22 -0300

    Add attribute for each type of category

--- app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionPresenter.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/addtransaction/presentation/AddTransactionPresenter.kt
@@ -4,13 +4,23 @@ import br.com.walter.walter.core.platform.CoroutinePresenter
 import br.com.walter.walter.core.functional.onFailure
 import br.com.walter.walter.core.functional.onSuccess
 import br.com.walter.walter.core.util.DateFormatter
+import br.com.walter.walter.features.addtransaction.Category
 import br.com.walter.walter.features.addtransaction.CategoryRepository
 import kotlinx.coroutines.launch
+
+const val EXPENSE_TYPE_ID = 1L
+const val INCOME_TYPE_ID = 2L
+const val INVESTMENT_TYPE_ID = 3L
 
 class AddTransactionPresenter(
     private val view: AddTransactionContract.View,
     private val categoryRepository: CategoryRepository
 ) : AddTransactionContract.Presenter, CoroutinePresenter() {
+
+    private var categories: List<Category>? = null
+    private var expenseCategories: List<Category>? = null
+    private var incomeCategories: List<Category>? = null
+    private var investmentCategories: List<Category>? = null
 
     override fun start() {
         getAllCategories()
@@ -20,7 +30,12 @@ class AddTransactionPresenter(
     override fun getAllCategories() {
         launch {
             categoryRepository.getAll()
-                .onSuccess { }
+                .onSuccess { allCategories ->
+                    categories = allCategories.filter { it.transactionTypeId == EXPENSE_TYPE_ID }
+                    expenseCategories = allCategories.filter { it.transactionTypeId == EXPENSE_TYPE_ID }
+                    incomeCategories = allCategories.filter { it.transactionTypeId == INCOME_TYPE_ID }
+                    investmentCategories = allCategories.filter { it.transactionTypeId == INVESTMENT_TYPE_ID }
+                }
                 .onFailure { view.showMessage("Failure!") }
         }
     }
