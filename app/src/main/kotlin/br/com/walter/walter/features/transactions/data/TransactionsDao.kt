4314d1f6db710e2b869e91096008fec77f44a688
Fernando Zabin <fernando.zabin@gmail.com>
2019-06-27 00:38:01 -0300

    Add method to get transactions between dates on transactions dao

--- app/src/main/kotlin/br/com/walter/walter/features/transactions/data/TransactionsDao.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/transactions/data/TransactionsDao.kt
@@ -3,11 +3,15 @@ package br.com.walter.walter.features.transactions.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
 
 @Dao
 interface TransactionsDao {
 
     @Insert(onConflict = REPLACE)
     fun save(TransactionDto: TransactionDto): Long
+
+    @Query("SELECT * FROM `transaction` WHERE date BETWEEN :startDate AND :endDate")
+    fun getTransactionsBetweenDates(startDate: String, endDate: String): List<TransactionDto>
 
 }
