a95c8de349a6b9bcf81144ec8959ebc2fbd24b34
Fernando Zabin <fernando.zabin@gmail.com>
2019-07-26 11:03:54 -0300

    Add transaction type id constants to transaction type domain model

--- app/src/main/kotlin/br/com/walter/walter/features/categories/domain/TransactionType.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/categories/domain/TransactionType.kt
@@ -1,5 +1,9 @@
 package br.com.walter.walter.features.categories.domain
 
+const val EXPENSE_TYPE_ID = 1L
+const val INCOME_TYPE_ID = 2L
+const val INVESTMENT_TYPE_ID = 3L
+
 data class TransactionType(
     val id: Long,
     val description: String
