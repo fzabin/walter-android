6ea252e26547d902bc006e819c632596c9cda805
Fernando Zabin <fernando.zabin@gmail.com>
2019-07-26 11:04:42 -0300

    Add properties to check transaction type to transaction with type domain model

--- app/src/main/kotlin/br/com/walter/walter/features/transactions/domain/TransactionWithType.kt
+++ app/src/main/kotlin/br/com/walter/walter/features/transactions/domain/TransactionWithType.kt
@@ -1,5 +1,8 @@
 package br.com.walter.walter.features.transactions.domain
 
+import br.com.walter.walter.features.categories.domain.EXPENSE_TYPE_ID
+import br.com.walter.walter.features.categories.domain.INCOME_TYPE_ID
+import br.com.walter.walter.features.categories.domain.INVESTMENT_TYPE_ID
 import java.math.BigDecimal
 
 data class TransactionWithType(
@@ -10,4 +13,15 @@ data class TransactionWithType(
     val description: String,
     val categoryId: Long,
     val transactionTypeId: Long
-)
+) {
+
+    val isExpense: Boolean
+        get() = transactionTypeId == EXPENSE_TYPE_ID
+
+    val isIncome: Boolean
+        get() = transactionTypeId == INCOME_TYPE_ID
+
+    val isInvestment: Boolean
+        get() = transactionTypeId == INVESTMENT_TYPE_ID
+
+}
